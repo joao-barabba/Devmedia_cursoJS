aafe0f26002dcf727654fd01d3bd549bdf371090
escreva o seu nome <jvbarabba@gmail.com>
2021-06-07 19:28:27 -0300

    :up: Exercicio 1 de Objeto Literal

---
+++ objetos/ex1.js
@@ -0,0 +1,13 @@
+let pessoa = {
+    nome: "João Victor Soares Nascimento",
+    idade: 23,
+    peso: 134
+}
+ let atendeAClassificacaoEtaria = pessoa.idade >= 18
+
+if(atendeAClassificacaoEtaria === true){
+    console.log("Idade permitida")
+
+}else{
+    console.log("Idade não permitida")
+}
