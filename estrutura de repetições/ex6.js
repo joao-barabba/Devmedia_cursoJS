a1aa25d009d2fe53d2b1359fd6f608db8ffc7f60
escreva o seu nome <jvbarabba@gmail.com>
2021-06-24 13:43:46 -0300

    :up: Exércio final de Estruturas de repetições

---
+++ estrutura de repetições/ex6.js
@@ -0,0 +1,29 @@
+let cursos_idiomas = [ // Exercício para percorrer dados de um objegto usando o FOR...OF
+    { nome: "Inglês", preco: 2500, carga_horaria: 160 },
+    { nome: "Espanhol", preco: 1500, carga_horaria: 110 },
+    { nome: "Francês", preco: 3600, carga_horaria: 200 },
+    { nome: "Chinês", preco: 5500, carga_horaria: 400 },
+    { nome: "Alemão", preco: 3800, carga_horaria: 230 }
+];
+
+for ( let curso of cursos_idiomas ) {
+
+    let nome_curso = curso.nome;
+    let preco_curso = curso.preco;
+    let carga_horaria_curso = curso.carga_horaria;
+
+    let preco_hora_curso = preco_curso / carga_horaria_curso;
+
+    console.log("Nome do curso: " + nome_curso);
+    console.log("Carga horária do curso: " + carga_horaria_curso + " horas");
+    console.log("Preço do curso: R$" + preco_curso + ",00");
+
+    if ( preco_hora_curso >= 15) {
+        console.log("Hora/aula superior ou igual a R$15,00");
+    } else {
+        console.log("Hora/aula inferior a R$15,00");
+    }
+
+    console.log("\n");
+
+}
